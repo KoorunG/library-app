d1085f4e93dd6e02ba1e20a742687a906726710a
KoorunG <onlyzion10@naver.com>
2023-07-26 23:05:40 +0900

    refactor : 계산의 결과를 검증하는 함수 추출 & 리팩토링

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -4,37 +4,35 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-/**
- * 계산기 테스트 클래스
- *
- * 1. 계산기는 정수만을 취급한다.
- * 2. 계산기가 생성될 때 숫자를 1개 받는다.
- * 3. 최초 숫자가 기록된 이후에는 연산자 함수를 통해 숫자를 받아 지속적으로 계산한다.
- */
+fun assertCalculatorEquality(
+    before: Int,
+    after: Int,
+    oper: Calculator.() -> Unit
+) {
+    // give
+    val calculator = Calculator(before)
+    // when
+    calculator.oper()
+    val expectedCalculator = Calculator(after)
+    // then
+    assertThat(calculator).isEqualTo(expectedCalculator)
+}
+
 class CalculatorTest {
 
     @Test
     fun `더하기 테스트`() {
-        // give
-        val calculator = Calculator(5)
-        // when
-        val result = calculator.add(3)
-        // then
-        assertThat(result).isEqualTo(8)
+        assertCalculatorEquality(5, 8) { add(3) }
     }
 
     @Test
     fun `빼기 테스트`() {
-        val calculator = Calculator(10)
-        val result = calculator.minus(5)
-        assertThat(result).isEqualTo(5)
+        assertCalculatorEquality(5, 2) { minus(3) }
     }
 
     @Test
     fun `곱하기 테스트`() {
-        val calculator = Calculator(5)
-        val result = calculator.multiply(3)
-        assertThat(result).isEqualTo(15)
+        assertCalculatorEquality(5, 15) { multiply(3) }
     }
 
     @Test
@@ -45,8 +43,6 @@ class CalculatorTest {
 
     @Test
     fun `나누기 테스트`() {
-        val calculator = Calculator(10)
-        val result = calculator.divide(5)
-        assertThat(result).isEqualTo(2)
+        assertCalculatorEquality(6, 2) { divide(3) }
     }
 }
