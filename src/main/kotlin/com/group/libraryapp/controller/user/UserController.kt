cae3f4383a781d5a443d2dd0d3ba3677301b3498
KoorunG <onlyzion10@naver.com>
2023-08-06 19:50:28 +0900

    feat : UserLoanHistoryResponse 생성 & 서비스, 컨트롤러에 관련 로직 등록

--- src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
+++ src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.controller.user
 
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserLoanHistoryResponse
 import com.group.libraryapp.service.user.UserService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -34,4 +35,6 @@ class UserController(
     fun deleteUser(@RequestParam name: String) {
         userService.deleteUser(name)
     }
+    @GetMapping("/loan")
+    fun getUserLoanHistories(): List<UserLoanHistoryResponse> = userService.getUserLoanHistories()
 }
--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -3,21 +3,22 @@ package com.group.libraryapp.service.user
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.UserStatus
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.BookHistoryResponse
+import com.group.libraryapp.dto.user.response.UserLoanHistoryResponse
 import com.group.libraryapp.dto.user.response.UserResponse
-import com.group.libraryapp.util.fail
 import com.group.libraryapp.util.findByIdOrThrow
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class UserService (
+class UserService(
     private val userRepository: UserRepository
 ) {
     @Transactional
-    fun saveUser(request: UserCreateRequest){
+    fun saveUser(request: UserCreateRequest) {
         userRepository.save(User(name = request.name, age = request.age, status = UserStatus.ACTIVE))
     }
 
@@ -43,4 +44,22 @@ class UserService (
             userRepository.delete(this)
         }
     }
+
+    // 유저의 대출기록을 가져오는 메소드
+    @Transactional(readOnly = true)
+    fun getUserLoanHistories(): List<UserLoanHistoryResponse> =
+        // 1. userRepository의 모든 유저 목록을 가져온다.
+        userRepository.findAll().map { user ->
+            // 2. User -> UserLoanHistoryResponse로 매핑한다.
+            UserLoanHistoryResponse(
+                name = user.name,
+                // 3. 여기서 books :: user에 걸린 책 대출내역을 가져와야 하므로 다시 map을 사용하여 매핑해야한다.
+                books = user.userLoanHistories.map { history ->
+                    BookHistoryResponse(
+                        name = history.bookName,
+                        // 4. 대출기록의 status를 Enum으로 관리하고 있기때문에 풀어준다.
+                        isReturn = history.status == UserLoanStatus.RETURNED
+                    )
+                })
+        }
 }
